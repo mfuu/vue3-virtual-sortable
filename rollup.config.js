b34793010dea2f1ea1c6d367d8fda9e5f6c8da02
mfuu <mfuu.me@gmail.com>
2026-04-08 11:03:09 +0800

    feat: umd -> cjs + esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,36 @@
 import babel from '@rollup/plugin-babel';
-import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
 const packageJson = require('./package.json');
-const version = packageJson.version;
-const homepage = packageJson.homepage;
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 const banner = `
 /*!
- * vue-virtual-sortable v${version}
+ * vue-virtual-sortable v${packageJson.version}
  * open source under the MIT license
- * ${homepage}
+ * ${packageJson.homepage}
  */
 `;
 
 export default [
   {
-    external: ['vue'],
     input: 'src/index.tsx',
     output: [
       {
-        format: 'umd',
-        file: 'dist/virtual-list.js',
-        name: 'VirtualList',
-        sourcemap: false,
-        globals: {
-          vue: 'Vue',
-        },
+        format: 'es',
+        file: packageJson.module,
         banner: banner.replace(/\n/, ''),
       },
       {
-        format: 'umd',
-        file: 'dist/virtual-list.min.js',
-        name: 'VirtualList',
-        sourcemap: false,
-        globals: {
-          vue: 'Vue',
-        },
+        format: 'cjs',
+        file: packageJson.main,
+        exports: 'default',
         banner: banner.replace(/\n/, ''),
-        plugins: [terser()],
       },
     ],
+    external: ['vue'],
     plugins: [resolve(), commonJs(), typescript(), babel({ extensions, babelHelpers: 'bundled' })],
   },
   {
